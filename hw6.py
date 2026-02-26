7fe6cb2bfbaa63b77998831d1964b05f1e297797
aizaleee <pamirbekovaaa@gmail.com>
2026-02-26 23:45:03 +0600

    hw6

--- hw6.py
+++ hw6.py
@@ -2,7 +2,11 @@ class User:
     num = [2, 7, 11, 15]
     target = 9
 
-    def two_sum(array, target):
-        for i in range(len[nums])
+    def twoSum(nums, target):
+        for i in range(len(nums)):             
+            for j in range(i + 1, len(nums)):  
+                if nums[i] + nums[j] == target:
+                    return [i, j]
 
-            
+    result = twoSum(num, target)
+    print(result)
